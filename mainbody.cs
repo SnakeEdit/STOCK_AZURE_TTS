3e146d14d8fa4d273d25a49656bf7eab1699fc4e
SnakeEdit <79651415+SnakeEdit@users.noreply.github.com>
2022-03-20 12:11:07 +0300

    adada
    adadadwdas

---
+++ mainbody.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
+namespace Merlin_spoke
+{
+    class mainbody
+    {
+        static async Task Main()
+        {
+            await SynthesizeAudioAsync();
+        }
+
+        static async Task SynthesizeAudioAsync()
+        {
+            var config = SpeechConfig.FromSubscription("ENTER_KEY_THERE", "ENTER_REGION_PROJECT_AZURE");
+            using var synthesizer = new SpeechSynthesizer(config);
+
+            var ssml = File.ReadAllText("./ssmlvoice.xml");
+            await synthesizer.SpeakSsmlAsync(ssml);
+        }
+    }
+}
+    
